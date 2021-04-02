100a7a9e74f020524129d231aa711837e9787a3b
benjiom <benjiom93@gmail.com>
2021-04-02 10:00:39 +0200

    Update function

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -219,7 +219,7 @@ class PdoGsb{
 		$dernierMois = $this->dernierMoisSaisi($idVisiteur);
 		$laDerniereFiche = $this->getLesInfosFicheFrais($idVisiteur,$dernierMois);
 		if($laDerniereFiche['idEtat']=='CR'){
-				$this->majEtatFicheFrais($idVisiteur, $dernierMois,'CL');
+				$this->majEtatFicheFrais($idVisiteur, $dernierMois,'CL', 1);
 
 		}
 		$req = "insert into FicheFrais(idVisiteur,mois,nbJustificatifs,montantValide,dateModif,idEtat)
